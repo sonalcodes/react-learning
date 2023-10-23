51a4195b9f0af3f2a61881bafc1f26dafb5419db
sonal grover <sonalgrover@sonals-MacBook-Air.local>
2023-10-23 13:31:25 +0530

    lesson 2

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h1", {}, "This is namaste react "),
     React.createElement("h2", {}, "I am an h2 Tag"),
   ]),
 
@@ -10,15 +12,6 @@ const parent = React.createElement("div", { id: "parent" }, [
   ]),
 ]);
 
-/*const heading = React.createElement(
-  "h1",
-  {
-    id: "heading",
-  },
-  "Hello world from React !"
-);*/
-
-//console.log(heading); // this is a javascript object , can also say react element
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent); // resposible to converting it to the h1 tag
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # React lesson one
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification (production build too)
+- Bundling
+- Compress
+- Consistent Hashing
+- Code splitting
+- Differential Bundling - support older browsers
+- Dianostic
+- Error Handling
+- HTTPS
+- Tree Shaking - remove unused code
+- Different Build for DEV and Production (creates different bundles for dev and production)
